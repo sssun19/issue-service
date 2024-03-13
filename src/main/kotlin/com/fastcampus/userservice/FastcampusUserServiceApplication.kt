015cc7e06b216cca332f7c2409f7e9e28ad6c5ff
sssun <98_0509@naver.com>
2024-03-13 23:41:27 +0900

    commit

--- src/main/kotlin/com/fastcampus/userservice/FastcampusUserServiceApplication.kt
+++ src/main/kotlin/com/fastcampus/userservice/FastcampusUserServiceApplication.kt
@@ -9,5 +9,5 @@ import org.springframework.boot.runApplication
 class FastcampusUserServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<FastcampusUserServiceApplication>(*args)
+    println("HelloWorld!")
 }
